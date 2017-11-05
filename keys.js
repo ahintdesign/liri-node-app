9882a4a189e184f5acc763e4dbb42dd838764780
Apple User <appleuser@mac-c82a14441071.attlocal.net>
2017-11-04 23:47:33 -0400

    hw files in progress

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<XGNulTRJ2h1IcmtN8EBAfzbjC>',
-  consumer_secret: '<aRmnJYSYroUFUKjDabuujRSvRLgnguGXGzhra8qZLRth4mSKaR>',
-  access_token_key: '<924679803223060480-6ujIpGurJSpuSsN3NKAHvRDRLszoEU1>',
-  access_token_secret: '<Uz1DQLxomeFHCayt3XESg5k20Q2iSYT5MdgCPG4O3WREk>',
+  consumer_key: 'XGNulTRJ2h1IcmtN8EBAfzbjC',
+  consumer_secret: 'aRmnJYSYroUFUKjDabuujRSvRLgnguGXGzhra8qZLRth4mSKaR',
+  access_token_key: '924679803223060480-6ujIpGurJSpuSsN3NKAHvRDRLszoEU1',
+  access_token_secret: 'Uz1DQLxomeFHCayt3XESg5k20Q2iSYT5MdgCPG4O3WREk',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -6,3 +6,70 @@
 // do-what-it-says
 
 
+var fs = require("fs");   
+var keys = require("./keys.js");
+var request = require('request');
+
+var Twitter = require("twitter");    
+// var spotify = require("spotify"); 
+var client = new Twitter(keys.twitterKeys); 
+
+var command = process.argv[2];  
+var value = process.argv[3]; 
+
+switch (command) {
+    case "my-tweets":
+        myTwitter();
+        
+
+}
+
+
+
+
+// // my-tweets function
+function myTwitter(){
+
+    var params = {
+        screen_name: 'AB-UNCC',
+        count: '20',
+   
+    }
+
+    // this is the call to twitter
+    client.get('statuses/user_timeline', params, function(err, timeline, response){
+        if(!err){
+            for(tweet in timeline){
+                //this creates the variable tdate which will store the result of the date from the twitter call for easier access later
+                var tDate = new Date(timeline[tweet].created_at);
+
+                //console.log all of the tweets organizing them by tweet# followed by the date of the tweet and finally the text of the tweet itself
+                console.log("Tweet #: " + (parseInt(tweet)+1) + " ");
+                console.log(tDate.toString().slice(0, 24) + " ");
+                console.log(timeline[tweet].text);
+                console.log("\n");
+
+                //append all of this information to the txt file 
+                fs.appendFile('log.txt', "Tweet #: " + (parseInt(tweet)+1) + "\n");
+                fs.appendFile('log.txt', timeline[tweet].text + "\n");
+                fs.appendFile('log.txt', "\n");
+
+            }
+        } 
+    })
+
+}
+
+
+
+
+// // spotify function             
+// function spotify-this-song () {
+
+// }; 
+
+
+// // movie-this function             
+// function spotify-this-song () {
+
+// }; 
